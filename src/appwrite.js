d464b5a9cd183c32c5270c5a2acc1433055ceecc
dishannath <dishan18nath@gmail.com>
2025-09-01 15:31:15 +0530

    Update appwrite.js

--- src/appwrite.js
+++ src/appwrite.js
@@ -1,5 +1,4 @@
-import { Client, Account, Databases, ID } from 'appwrite';
-
+import { Client, Account, Databases, ID, Functions } from 'appwrite';
 export { ID } from 'appwrite';
 
 const client = new Client();
@@ -10,5 +9,6 @@ client
 
 export const account = new Account(client);
 export const databases = new Databases(client);
+export const functions = new Functions(client);
 
 export default client;
